8f336960efae5b8dc559ece5f368aa7cdeb5037f
Jeff Day <jeffrey.day00@gmail.com>
2019-05-09 14:36:30 -0700

    Set up environment and rendered react app to the DOM.

---
+++ challenge_4/client/src/components/app.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+class App extends React.Component {
+
+  render(){
+    return (
+        <div>
+            <div>Hello from React App</div>
+            <div>Hello twice</div>
+            <div>Hello thrice</div>
+        </div>
+    )
+  }
+}
+
+export default App;
--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,11 @@
+var express = require('express');
+var app = express();
 
+app.use(express.static('./client/dist'))
+
+app.get('/', function(req, res){
+  res.send('hello here');
+});
+
+app.listen(3000, console.log('Server is connected at 3000'));
 
